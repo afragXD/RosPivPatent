598bbb45f8829e2eeb309d7697619100c17dffed
Mergen <tiuleev@sfedu.ru>
2022-09-21 02:12:02 +0300

    +menu везде

--- app/src/main/java/com/example/rospivpatent/SearchingResultsActivity.java
+++ app/src/main/java/com/example/rospivpatent/SearchingResultsActivity.java
@@ -1,14 +1,34 @@
 package com.example.rospivpatent;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
 
 public class SearchingResultsActivity extends AppCompatActivity {
-
+    private ImageButton btnLoup, btnMenu;
+    DrawerLayout drawerLayout;
+    private LinearLayout simpleSearch, advancedSearch, AISearch, options;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_searching_results);
+        init();
+        click();
+    }
+    private void init(){
+        btnMenu = findViewById(R.id.btnMenu_1);
+    }
+    private void click(){
+        btnMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                drawerLayout.openDrawer(GravityCompat.START);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/rospivpatent/SimpleSearchActivity.java
+++ app/src/main/java/com/example/rospivpatent/SimpleSearchActivity.java
@@ -1,21 +1,29 @@
 package com.example.rospivpatent;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
 
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
 import android.widget.Spinner;
 import android.widget.TextView;
 
 public class SimpleSearchActivity extends AppCompatActivity {
-
+    private ImageButton  btnMenu;
+    DrawerLayout drawerLayout;
+    private LinearLayout simpleSearch, advancedSearch, AISearch, options;
     String[] countries = { "Россия и страны СНГ", "Минимум РСТ", "Страны с малым потентным фондом"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        init();
+        click();
         setContentView(R.layout.activity_simple_search);
         Spinner spinner = findViewById(R.id.spinner);
         ArrayAdapter<String> adapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item, countries);
@@ -35,5 +43,16 @@ public class SimpleSearchActivity extends AppCompatActivity {
             }
         };
         spinner.setOnItemSelectedListener(itemSelectedListener);
+    }
+    private void init(){
+        btnMenu = findViewById(R.id.btnMenu_2);
+    }
+    private void click(){
+        btnMenu.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            drawerLayout.openDrawer(GravityCompat.START);
+        }
+    });
     }
 }
